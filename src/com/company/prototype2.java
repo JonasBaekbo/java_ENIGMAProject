bcea105641461065e4244a4bcef5f4100b56304c
JonasBaekbo <jonas.bryde.baekbo@live.dk>
2021-09-14 14:12:19 +0200

    Prototype 2 nu med textToNumbersList

--- src/com/company/prototype2.java
+++ src/com/company/prototype2.java
@@ -1,13 +1,20 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class prototype2 {
     public static void main(String[] args) {
-        char letter = 'B';
-        int number = letterToNumber(letter);
-        System.out.printf("Bogstavet %c bliver til %d", letter, number);
+        String plainText = "ABE";
+        int[] cipherText = textToNumbersList(plainText);
+        System.out.println(Arrays.toString(cipherText));
     }
     public static int[] textToNumbersList(String text){
-        return new int[0];
+        int[] list = new int[text.length()];
+        for (int i = 0; i<text.length(); i++){
+            list[i] = letterToNumber(text.charAt(i));
+        }
+
+        return list;
     }
     public static int letterToNumber(char letter){
         String theAlphabet = " ABCDEFGHIJKLMNOPQRSTUVXYZÆØÅ";
