2c1b94b6cc61f1b035f5d9c5d32d658b45079edf
JonasBaekbo <jonas.bryde.baekbo@live.dk>
2021-09-14 14:41:43 +0200

    Prototype 2 nu med numberToLetter og numbersListToText

--- src/com/company/prototype2.java
+++ src/com/company/prototype2.java
@@ -4,9 +4,16 @@ import java.util.Arrays;
 
 public class prototype2 {
     public static void main(String[] args) {
+        int cipher[] = {1,2,5};
+        String plainText = numbersListtoText(cipher);
+        System.out.println(plainText);
+
+        /*
+        Text To Number
         String plainText = "ABE";
         int[] cipherText = textToNumbersList(plainText);
         System.out.println(Arrays.toString(cipherText));
+        */
     }
     public static int[] textToNumbersList(String text){
         int[] list = new int[text.length()];
@@ -21,5 +28,23 @@ public class prototype2 {
         int number = theAlphabet.indexOf(letter);
 
         return number;
+    }
+
+
+    public static char numberToLetter(int number){
+        String theAlphabet = " ABCDEFGHIJKLMNOPQRSTUVXYZÆØÅ";
+        char letter = theAlphabet.charAt(number);
+        return letter;
+    }
+
+    public static String numbersListtoText(int[] numbers){
+        String text = "";
+        for (int i=0; i<numbers.length; i++){
+            int number = numbers[i];
+
+            char letter = numberToLetter(number);
+            text = text + letter;
+        }
+        return text;
     }
 }
